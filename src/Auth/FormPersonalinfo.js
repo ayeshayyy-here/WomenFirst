347db02b4aba278797d444d517bfcea7706acc5e
Usama ijaz <84039132+Usama894@users.noreply.github.com>
2024-08-13 10:33:15 +0500

    Update FormPersonalinfo.js

--- src/Auth/FormPersonalinfo.js
+++ src/Auth/FormPersonalinfo.js
@@ -6,40 +6,46 @@ import {
   StyleSheet,
   ScrollView,
   TouchableOpacity,
+  Modal,
+  TouchableWithoutFeedback,
+  PermissionsAndroid,
+  Image,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import ProgressBar from '../components/ProgressBar';
 import { useNavigation } from '@react-navigation/native';
 import { DatePickerInput } from 'react-native-paper-dates';
 import syncStorage from 'react-native-sync-storage';
+import DocumentPicker from 'react-native-document-picker';
+import { launchCamera } from 'react-native-image-picker';
 
 const FormP = () => {
-  const [dob, setDOB] = useState('');
-  const [expiry, setExpiry] = useState('');
-  const [issuance, setIssuance] = useState('');
   const [formData, setFormData] = useState({
     name: '',
     phone: '',
     cnic: '',
     datee: '',
     dateb: '',
-    jobTitle: '',
+    datei: '',
+    jobheld: '',
+    serving:'',
     jobStartDate: '',
     salary: '',
     address: '',
     mobile: '',
     disability: '',
   });
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedAttachment, setSelectedAttachment] = useState('');
+  const [capturedImage, setCapturedImage] = useState(null);
+  const [stateFunctions, setStateFunctions] = useState({});
 
   const navigation = useNavigation();
 
-  // Load form data from sync storage on component mount
   useEffect(() => {
     const savedData = syncStorage.get('formData');
     if (savedData) {
       setFormData(savedData);
-      setDOB(savedData.dateb);
-      setExpiry(savedData.datee);
-      setIssuance(savedData.datei);
     }
   }, []);
 
@@ -52,10 +58,80 @@ const FormP = () => {
     syncStorage.set('formData', formData);
     navigation.navigate('FormG');
   };
+
+  const openCamera = async () => {
+    setModalVisible(false);
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA,
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      const options = {
+        mediaType: 'photo',
+        includeBase64: true,
+        maxHeight: 2000,
+        maxWidth: 2000,
+      };
+
+      launchCamera(options, response => {
+        if (response.didCancel) {
+          console.log('User cancelled camera');
+        } else if (response.error) {
+          console.log('Camera Error: ', response.error);
+        } else {
+          const fileName = response.assets[0].fileName;
+          const imageUri = response.assets[0].uri;
+
+          setCapturedImage(imageUri);
+          setStateFunctions(prev => ({
+            ...prev,
+            [selectedAttachment]: { Name: fileName, URI: imageUri, Type: 'image' },
+          }));
+        }
+      });
+    }
+  };
+
+  const openGallery = async () => {
+    try {
+      const response = await DocumentPicker.pick({
+        allowMultiSelection: false,
+        type: [DocumentPicker.types.images], // This filters to only allow images (PNG, JPG, JPEG)
+      });
+  
+      const fileType = response[0].type;
+      const allowedTypes = ['image/png', 'image/jpeg', 'image/jpg'];
+  
+      if (allowedTypes.includes(fileType)) {
+        setStateFunctions(prev => ({
+          ...prev,
+          [selectedAttachment]: {
+            Name: response[0].name,
+            URI: response[0].uri,
+            Type: fileType,
+          },
+        }));
+      } else {
+        alert('Only PNG, JPG, and JPEG files are allowed.');
+      }
+  
+      setModalVisible(false);
+    } catch (error) {
+      if (DocumentPicker.isCancel(error)) {
+        console.log('User canceled the file selection');
+      } else {
+        console.error('Document picking error:', error);
+      }
+    }
+  };
   
 
+  const handleUploadClick = (attachmentName) => {
+    setSelectedAttachment(attachmentName);
+    setModalVisible(true);
+  };
+
   return (
-    <ScrollView contentContainerStyle={styles.container}>
+    <ScrollView contentContainerStyle={styles.screenContainer}>
       <Text style={styles.header}>Application Form</Text>
       <ProgressBar step={1} />
 
@@ -63,6 +139,26 @@ const FormP = () => {
       <View style={styles.section}>
         <Text style={styles.sectionHeader}>Personal Information</Text>
         <View style={styles.divider} />
+        
+        {/* Display image or upload icon based on capturedImage */}
+        {!capturedImage && !stateFunctions[selectedAttachment]?.URI ? (
+          <View style={styles.iconWrapper}>
+            <Text style={styles.iconText}>Choose or Upload Profile Image</Text>
+            <TouchableOpacity
+              onPress={() => handleUploadClick('profileImage')}
+              style={styles.iconWrapper}>
+              <Icon name="upload" size={30} color="black" />
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <View style={styles.imageContainer}>
+            <Image
+              source={{ uri: capturedImage || stateFunctions[selectedAttachment]?.URI }}
+              style={styles.image}
+            />
+          </View>
+        )}
+        
         <Text style={styles.text}>Applicant's Name:</Text>
         <TextInput
           style={styles.input}
@@ -115,56 +211,45 @@ const FormP = () => {
           onChangeText={text => handleInputChange('cnic', text)}
         />
         <Text style={[styles.text, { marginTop: '5%' }]}>Date of Expiry:</Text>
-        <TouchableOpacity
-          style={{
-            marginTop: 5,
-            backgroundColor: '#fff',
-            borderRadius: 5,
-            height: 40,
-          }}>
+        <TouchableOpacity style={styles.datePickerWrapper}>
           <DatePickerInput
             locale="en"
             label="" // No value provided for label
-            value={expiry}
-            onChange={expiry => {
-              setExpiry(expiry);
-              handleInputChange('datee', expiry);
+            value={formData.datee}
+            onChange={datee => {
+              setFormData(prev => ({ ...prev, datee }));
             }}
             mode={'flat'}
             style={styles.calenderstyle}
           />
         </TouchableOpacity>
         <Text style={[styles.text, { marginTop: '5%' }]}>Date of Birth:</Text>
-        <TouchableOpacity
-          style={{ backgroundColor: '#fff', borderRadius: 5, height: 40 }}>
+        <TouchableOpacity style={styles.datePickerWrapper}>
           <DatePickerInput
             locale="en"
             label="" // No value provided for label
-            value={dob}
-            onChange={dob => {
-              setDOB(dob);
-              handleInputChange('dateb', dob);
+            value={formData.dateb}
+            onChange={dateb => {
+              setFormData(prev => ({ ...prev, dateb }));
             }}
             mode={'flat'}
             style={styles.calenderstyle}
           />
         </TouchableOpacity>
         <Text style={[styles.text, { marginTop: '5%' }]}>Date of Issue:</Text>
-        <TouchableOpacity
-          style={{ backgroundColor: '#fff', borderRadius: 5, height: 40 }}>
+        <TouchableOpacity style={styles.datePickerWrapper}>
           <DatePickerInput
             locale="en"
             label="" // No value provided for label
-            value={issuance}
-            onChange={issuance => {
-              setIssuance(issuance);
-              handleInputChange('datei', issuance);
+            value={formData.datei}
+            onChange={datei => {
+              setFormData(prev => ({ ...prev, datei }));
             }}
             mode={'flat'}
             style={styles.calenderstyle}
           />
         </TouchableOpacity>
-        <Text style={[styles.text, { marginTop: 10 }]}>
+        <Text style={[styles.text, { marginTop: 20 }]}>
           Any Physical Disability:
         </Text>
         <TextInput
@@ -183,28 +268,82 @@ const FormP = () => {
         <Text style={styles.text}>Post Held:</Text>
         <TextInput
           style={styles.input}
-          placeholder="Enter post held"
+          placeholder="Enter Post"
           placeholderTextColor="grey"
-          value={formData.jobTitle}
+          value={formData.jobheld}
           onChangeText={text => handleInputChange('jobTitle', text)}
         />
-        <Text style={styles.text}>Since When Working on Current Job:</Text>
+         <Text style={styles.text}>Since When Serving on Current Job:</Text>
         <TextInput
           style={styles.input}
-          placeholder="Enter start date"
+          placeholder="Enter Current Job"
           placeholderTextColor="grey"
-          value={formData.jobStartDate}
-          onChangeText={text => handleInputChange('jobStartDate', text)}
+          value={formData.serving}
+          onChangeText={text => handleInputChange('jobTitle', text)}
         />
-        <Text style={styles.text}>Salary:</Text>
+        <Text style={styles.text}>Job Start Date:</Text>
+        <TouchableOpacity style={styles.datePickerWrapper}>
+          <DatePickerInput
+            locale="en"
+            label="" // No value provided for label
+            value={formData.jobStartDate}
+            onChange={jobStartDate => {
+              setFormData(prev => ({ ...prev, jobStartDate }));
+            }}
+            mode={'flat'}
+            style={styles.calenderstyle}
+          />
+        </TouchableOpacity>
+        <Text style={[styles.text, { marginTop: 20 }]}>Salary:</Text>
         <TextInput
           style={styles.input}
-          placeholder="Enter salary"
+          placeholder="Enter Salary"
+          keyboardType="numeric"
           placeholderTextColor="grey"
           value={formData.salary}
           onChangeText={text => handleInputChange('salary', text)}
         />
+        <Text style={[styles.text]}>Job Type:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Select Type"
+          placeholderTextColor="grey"
+          value={formData.salary}
+          onChangeText={text => handleInputChange('salary', text)}
+        />
+         <Text style={[styles.sectionHeader,{marginTop:10}]}>Duty Hours in Summer</Text>
+         <View style={styles.divider} />
       </View>
+
+      <Modal
+        visible={modalVisible}
+        transparent={true}
+        animationType="fade"
+        onRequestClose={() => setModalVisible(false)}>
+        <TouchableWithoutFeedback onPress={() => setModalVisible(false)}>
+          <View style={styles.modalOverlay}>
+            <TouchableWithoutFeedback>
+              <View style={styles.modalContent}>
+                <Text style={styles.modalTitle}>Choose an option</Text>
+                <View style={styles.modalOptionsRow}>
+                  <TouchableOpacity
+                    style={styles.modalButton}
+                    onPress={openCamera}>
+                    <Icon name="camera" size={30} color="black" />
+                    <Text style={styles.modalButtonText}>Capture Image</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={styles.modalButton}
+                    onPress={openGallery}>
+                    <Icon name="file" size={30} color="black" />
+                    <Text style={styles.modalButtonText}>Upload File</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
 
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.button} onPress={handleNextPress}>
@@ -216,18 +355,30 @@ const FormP = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
+  screenContainer: {
     padding: 16,
     backgroundColor: '#f8f8f8',
+    flexGrow: 1,
   },
   divider: {
     height: 0.2,
     backgroundColor: 'grey',
     marginVertical: 10,
-    width: '90%', // Set the width of the divider (e.g., 80% of the container's width)
-    alignSelf: 'center', // Center the divider within its container
+    width: '90%',
+    alignSelf: 'center',
   },
-
+  iconWrapper: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 10,
+    marginVertical: 20,
+  },
+  iconText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: 'black',
+  },
   header: {
     fontSize: 20,
     fontWeight: 'bold',
@@ -259,11 +410,10 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 12,
     fontWeight: 'bold',
-    marginBottom: 8, // Adjust this value to control the space between text and TextInput
+    marginBottom: 8,
     marginTop: 5,
     color: 'black',
   },
-
   input: {
     flex: 1,
     color: 'black',
@@ -272,7 +422,7 @@ const styles = StyleSheet.create({
     height: 40,
     borderWidth: 0.2,
     borderColor: 'grey',
-    marginBottom: 8, // Adds space between each TextInput
+    marginBottom: 8,
     paddingLeft: 10,
     fontSize: 12,
   },
@@ -291,6 +441,71 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 14,
     fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  datePickerWrapper: {
+    marginTop: 5,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    height: 40,
+  },
+  modalOverlay: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    padding: 20,
+    paddingBottom: 30,
+    alignItems: 'center',
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  modalOptionsRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+  modalButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+  },
+  modalButtonText: {
+    color: '#010048',
+    fontSize: 16,
+    marginLeft: 10,
+    marginTop: 10,
+  },
+  imageContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+    borderRadius: 50, // Rounded circle container
+    width: 100,
+    height: 100,
+    borderWidth: 2,
+    borderColor: '#fff',
+    overflow: 'hidden',
+    alignSelf: 'center', // Center the image container horizontally
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    borderRadius: 50, // Ensure the image is circular
+  },
+  fileNameText: {
+    fontSize: 14,
+    marginTop: 10,
+    color: 'black',
     textAlign: 'center',
   },
 });
